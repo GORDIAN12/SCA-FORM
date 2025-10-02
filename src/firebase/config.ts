c830caca9503a3173228aa2f3f51634948186844
David Gordian <davidgordian667@gmail.com>
2025-10-02 18:20:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2081043830-ec933",
+  "appId": "1:406281301821:web:ff5313eaf9ecb305b29f54",
+  "apiKey": "AIzaSyCj9m4ZiuODBxSly8OuDd7r0t5a1dq3NVU",
+  "authDomain": "studio-2081043830-ec933.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "406281301821"
+};
