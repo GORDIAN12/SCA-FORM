3f6ac4a93b833973d98a52d69cee20570eba9bc0
David Gordian <davidgordian667@gmail.com>
2025-12-19 05:08:41 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-2081043830-ec933",
-  "appId": "1:406281301821:web:ff5313eaf9ecb305b29f54",
-  "apiKey": "AIzaSyCj9m4ZiuODBxSly8OuDd7r0t5a1dq3NVU",
-  "authDomain": "studio-2081043830-ec933.firebaseapp.com",
+  "projectId": "cupping-compassrespa-371-ab979",
+  "appId": "1:925529683382:web:93ac1a6e90ff976b04bd17",
+  "apiKey": "AIzaSyCYLtB8kKgw-EqhJG1fVnh5p6_-u-_vXWg",
+  "authDomain": "cupping-compassrespa-371-ab979.firebaseapp.com",
   "measurementId": "",
-  "messagingSenderId": "406281301821"
+  "messagingSenderId": "925529683382"
 };
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,7 +3,7 @@
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore'
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
@@ -19,11 +19,8 @@ export function initializeFirebase() {
     } catch (e) {
       // Only warn in production because it's normal to use the firebaseConfig to initialize
       // during development
-      if (process.env.NODE_ENV === 'production') {
-        console.warn(
-          'Automatic initialization failed. Falling back to firebase config object.',
-          e
-        );
+      if (process.env.NODE_ENV === "production") {
+        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
       }
       firebaseApp = initializeApp(firebaseConfig);
     }
@@ -36,11 +33,10 @@ export function initializeFirebase() {
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
-  const firestore = getFirestore(firebaseApp);
   return {
     firebaseApp,
     auth: getAuth(firebaseApp),
-    firestore: firestore,
+    firestore: getFirestore(firebaseApp)
   };
 }
 
@@ -48,4 +44,7 @@ export * from './provider';
 export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
-
+export * from './non-blocking-updates';
+export * from './non-blocking-login';
+export * from './errors';
+export * from './error-emitter';
